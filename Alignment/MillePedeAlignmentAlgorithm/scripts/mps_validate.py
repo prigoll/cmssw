d71c01e1949f7ef232df11160e617c0362f263c0
Philipp Rigoll <uhdyr@student.kit.edu>
2016-05-18 11:29:04 +0200

    plot big structures

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_validate.py
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_validate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from ROOT import TTree, TFile, TH1F, TCanvas
+from ROOT import TTree, TFile, TH1F, TCanvas, TImage, TPaveLabel
 import logging
 
 class GeometryGetter:
@@ -23,15 +23,68 @@ def main():
     
     geometrygetter = GeometryGetter()
     
-    treeFile = TFile("./jobData/jobm/treeFile_merge.root")
-    MillePedeUser1 = treeFile.Get("MillePedeUser_1")
+    # TODO get latest Alignment directory
+    alignmentNumber = 3
     
+    # TODO which time MillePedeUser_X
+    alignmentTime = 1
+    
+    
+    # TODO check of there is a file and a TTree
+    # open root file and get TTree MillePedeUser_X
+    if (alignmentNumber == 0):
+        treeFile = TFile("./jobData/jobm/treeFile_merge.root")
+    else:
+        treeFile = TFile("./jobData/jobm{0}/treeFile_merge.root".format(alignmentNumber))
+    MillePedeUser = treeFile.Get("MillePedeUser_{0}".format(alignmentTime))
+    
+    
+    # big structures
+    
+    cBigXYZ = TCanvas("canvasBigStrucutres", "Parameter", 300, 0, 800, 600)
+    cBigXYZ.Divide(2,2)
+    
+    # TODO dynamic range, error
+    hBigX = TH1F("Big Structure X", "Parameter X", 100, -0.005, 0.005)
+    hBigY = TH1F("Big Structure Y", "Parameter Y", 100, -0.005, 0.005)
+    hBigZ = TH1F("Big Structure Z", "Parameter Z", 100, -0.005, 0.005)
+    hBigX.SetXTitle("[cm]")
+    hBigY.SetXTitle("[cm]")
+    hBigZ.SetXTitle("[cm]")
+    
+    title = TPaveLabel(0.1,0.2,0.3,0.4, "Big Structures")
+    
+    for line in MillePedeUser:
+        if (line.ObjId != 1):
+            hBigX.Fill(line.Par[0])
+            hBigY.Fill(line.Par[1])
+            hBigZ.Fill(line.Par[2])
+    
+    cBigXYZ.cd(1)
+    title.Draw()
+    cBigXYZ.cd(2)
+    hBigX.Draw()
+    cBigXYZ.cd(3)
+    hBigY.Draw()
+    cBigXYZ.cd(4)
+    hBigZ.Draw()
+    cBigXYZ.Update()
+    
+    imageBig = TImage.Create()
+    imageBig.FromPad(cBigXYZ)
+    imageBig.WriteImage("BigXYZ.png")
+    
+    input("wait...")
+    
+    """
     try:
         for element in MillePedeUser1:
             print "Id: {0} ({1})".format(element.Id, geometrygetter.name_by_objid(element.ObjId))
     except Exception as e:
         logging.critical("Error: {}".format(e))
         raise
+    """    
+    
 
     
 if __name__ == "__main__":
