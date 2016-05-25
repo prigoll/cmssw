bc2c719f70a0c5408d9e77fac3cb584c8f788f78
Philipp Rigoll <philipp@rigoll.de>
2016-05-25 14:51:02 +0200

    if the range is to big use 3 times the StdDev

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_validate.py
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_validate.py
@@ -44,6 +44,7 @@ class TreeData:
         self.numberOfBins = [0, 0, 0]
         self.maxShift = [0, 0, 0]
         self.maxBinShift = [0, 0, 0]
+        self.hiddenEntries = [0, 0, 0]
         self.binPosition = [1, 1, 1]
         self.histo = []
         self.histoAxis = []
@@ -319,36 +320,56 @@ def main():
             # skip empty histogram
             if (mod.maxBinShift[i] == numberOfBins/2+1):
                 mod.maxBinShift[i] = 0
+            # set the maxShift value
+            mod.maxShift[i] = mod.histo[i].GetBinCenter( numberOfBins/2+mod.maxBinShift[i] )
 
-        # apply the new range
+        # find and apply the new range
         for i in range(3):
-            
-            # merge bins, ca. 100 should be visible in the resulting plot
-            mergeNumberBins = max(mod.maxBinShift)
-            # skip empty histogram
-            if (mergeNumberBins != 0):
-                # the 2*maxBinShift bins should shrink to 100 bins
-                mergeNumberBins = int(2*mergeNumberBins/100.)
-                # the total number of bins should be dividable by the bins shrinked together
-                while (numberOfBins%mergeNumberBins != 0):
-                    mergeNumberBins -= 1
+            if (mod.histo[i].GetEntries() != 0 and mod.histo[i].GetStdDev() != 0):
+                # if the plotrange is much bigger than the standard deviation use 3 * StdDev als Range
+                if ( max(mod.maxShift)/mod.histo[i].GetStdDev() > 9 ):
+                    # corresponding bin 3*StdDev
+                    binShift = int(mod.histo[i].FindBin(3*mod.histo[i].GetStdDev()) - numberOfBins/2)
+                    # count entries which are not shown anymore
+                    # bin 1 to begin of histogram
+                    for j in range(1, numberOfBins/2 - binShift):
+                        mod.hiddenEntries[i] += mod.histo[i].GetBinContent(j)
+                    # from the end of shown bins to the end of histogram
+                    for j in range(numberOfBins/2 + binShift, mod.histo[i].GetNbinsX()):
+                        mod.hiddenEntries[i] += mod.histo[i].GetBinContent(j)
+                else:
+                    binShift = max(mod.maxBinShift)
                 
-                # Rebin and save new created histogram and axis
-                mod.histo[i] = mod.histo[i].Rebin( mergeNumberBins )
-                mod.histoAxis[i] = mod.histo[i].GetXaxis()
-                
-                # set view range. it is important to note that the number of bins have changed with the rebinning
-                # the total number and the number of shift must be corrected with / mergeNumberBins
-                mod.histoAxis[i].SetRange(int(numberOfBins/(2*mergeNumberBins)-max(mod.maxBinShift) / mergeNumberBins), int(numberOfBins/(2*mergeNumberBins)+max(mod.maxBinShift) / mergeNumberBins))
+                # merge bins, ca. 100 should be visible in the resulting plot
+                mergeNumberBins = binShift
+                # skip empty histogram
+                if (mergeNumberBins != 0):
+                    # the 2*maxBinShift bins should shrink to 100 bins
+                    mergeNumberBins = int(2*mergeNumberBins/100.)
+                    # the total number of bins should be dividable by the bins shrinked together
+                    if (mergeNumberBins == 0):
+                        mergeNumberBins = 1
+                    while (numberOfBins%mergeNumberBins != 0 and mergeNumberBins != 1):
+                        mergeNumberBins -= 1
+                    
+                    # Rebin and save new created histogram and axis
+                    mod.histo[i] = mod.histo[i].Rebin( mergeNumberBins )
+                    mod.histoAxis[i] = mod.histo[i].GetXaxis()
+                    
+                    # set view range. it is important to note that the number of bins have changed with the rebinning
+                    # the total number and the number of shift must be corrected with / mergeNumberBins
+                    mod.histoAxis[i].SetRange(int(numberOfBins/(2*mergeNumberBins)-binShift / mergeNumberBins), int(numberOfBins/(2*mergeNumberBins)+binShift / mergeNumberBins))
             
                
         # TODO chose good limit
         # error if shift is bigger than limit
         limit = 0.02
         for i in range(3):
-            text.AddText("max. shift {0}: {1:.2}".format(mod.xyz[i], mod.histoAxis[i].GetBinCenter(mod.maxBinShift[i])))
-            if (mod.histoAxis[i].GetBinCenter(mod.maxBinShift[i]) > limit):
+            text.AddText("max. shift {0}: {1:.2}".format(mod.xyz[i], mod.maxShift[i]))
+            if (mod.maxShift[i] > limit):
                 text.AddText("! {0} shift bigger than {1} !".format(mod.xyz[i], limit))
+            if (mod.hiddenEntries[i] != 0):
+                text.AddText("! {0} entries not shown !".format(int(mod.hiddenEntries[i])))
         
         
                         
